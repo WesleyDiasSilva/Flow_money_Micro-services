00c3b9b84ba939cbaafbefca82fa8db8f234e5ec
Wesley Dias <wesley.dias.dev@gmail.com>
2023-04-15 09:48:18 -0300

    fix: send email for new user

--- notifier/src/app.controller.ts
+++ notifier/src/app.controller.ts
@@ -5,9 +5,11 @@ import { NewNotification } from './dtos/new.notification.dto';
 
 @Controller()
 export class AppController {
+  logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
   @MessagePattern('notify')
   async handleNotification(@Payload() message: NewNotification) {
+    this.logger.log(message);
     return await this.appService.verifyTypes(message);
   }
 }
